b75daa74fec5e0e17859bee88af8a7497d9e680b
unknown <timothy.kage@gmail.com>
2021-06-16 23:37:28 +0200

    Added comments and conformed to PEP8

--- sudoku.py
+++ sudoku.py
@@ -1,24 +1,31 @@
 import csv
 
 
-
 matrix = []
 rows = []
 empty_pos_list = []
 subscribing_functions = []
 
+
+# Trigger for notifying subscribers of changes to matrix
 def notify_subscribers(input_matrix):
     for sub in subscribing_functions:
         sub(input_matrix)
-        
+
+
+# Appends function to list of functions subscribing to matrix changes
 def subscribe_to_matrix_changes(func):
     subscribing_functions.append(func)
 
+
+# Prints matrix
 def print_matrix():
     for x in matrix:
         "".join(str(x))
         print(x)
 
+
+# Fills game matrix from sudoku.csv
 def fill_game_matrix_from_CSV():
 
     with open("sudoku.csv") as f:
@@ -26,63 +33,63 @@ def fill_game_matrix_from_CSV():
         for row in reader:
             list_row = list(row)
             matrix.append(list_row)
-            
 
+
+# Sets matrix
 def set_matrix(input_matrix):
     global matrix
     matrix = input_matrix
-    
+
+
+# Returns matrix
 def get_matrix():
     return matrix
-            
 
+
+# Checks if a digit is valid in particular position within a matrix
 def check_if_valid(matrix, pos, digit):
-    
-    # Checks for fixed x-position 
+
+    # Checks for fixed x-position
     for y in range(len(matrix)):
 
         if matrix[y][pos[1]] == str(digit):
 
             return False
-                
-               
+
     # Checks for fixed y-position
     for x in range(len(matrix)):
 
         if matrix[pos[0]][x] == str(digit):
 
             return False
-            
-    #Check box 1
+
+    # Check box 1
     if pos[0] in range(0, 3) and pos[1] in range(0, 3):
 
-        
         for x in range(0, 3):
             for y in range(0, 3):
                 if matrix[x][y] == str(digit):
 
                     return False
-    #Check box 2             
-    elif pos[0] in range(3,6) and pos[1] in range(0, 3):
+    # Check box 2
+    elif pos[0] in range(3, 6) and pos[1] in range(0, 3):
 
-        
         for x in range(3, 6):
             for y in range(0, 3):
                 if matrix[x][y] == str(digit):
 
                     return False
-            
-            
-    #Check box 3 
-    elif pos[0] in range(6,9) and pos[1] in range(0, 3):
+
+    # Check box 3
+    elif pos[0] in range(6, 9) and pos[1] in range(0, 3):
 
         for x in range(6, 9):
             for y in range(0, 3):
                 if matrix[x][y] == str(digit):
 
                     return False
-                    
-    #Check box 4        
+
+    # Check box 4
     elif pos[0] in range(0, 3) and pos[1] in range(3, 6):
 
         for x in range(0, 3):
@@ -90,8 +97,8 @@ def check_if_valid(matrix, pos, digit):
                 if matrix[x][y] == str(digit):
 
                     return False
-                 
-    #Check box 5
+
+    # Check box 5
     elif pos[0] in range(3, 6) and pos[1] in range(3, 6):
 
         for x in range(3, 6):
@@ -99,8 +106,8 @@ def check_if_valid(matrix, pos, digit):
                 if matrix[x][y] == str(digit):
 
                     return False
-                    
-    #Check box 6
+
+    # Check box 6
     elif pos[0] in range(6, 9) and pos[1] in range(3, 6):
 
         for x in range(6, 9):
@@ -108,45 +115,44 @@ def check_if_valid(matrix, pos, digit):
                 if matrix[x][y] == str(digit):
 
                     return False
-                    
-                    
-    #Check box 7
+
+    # Check box 7
     elif pos[0] in range(0, 3) and pos[1] in range(6, 9):
 
         for x in range(0, 3):
             for y in range(6, 9):
                 if matrix[x][y] == str(digit):
-                    
+
                     return False
-                    
-    #Check box 8                
+
+    # Check box 8
     elif pos[0] in range(3, 6) and pos[1] in range(6, 9):
 
         for x in range(3, 6):
             for y in range(6, 9):
                 if matrix[x][y] == str(digit):
-                    
+
                     return False
-                    
-    #Check box 9                
+
+    # Check box 9
     elif pos[0] in range(6, 9) and pos[1] in range(6, 9):
 
         for x in range(6, 9):
             for y in range(6, 9):
                 if matrix[x][y] == str(digit):
-                    
-                    return False               
-                    
+
+                    return False
+
     return True
-            
-           
+
+
+# Finds empty position in matrix and attempts to input digit in empty
+# position. Returns true if matrix is solved.
 def solve_matrix(matrix):
     empty_pos = find_empty_position(matrix)
     if not empty_pos:
         return True
-    
 
-    
     for digit in range(1, 10):
 
         if check_if_valid(matrix, empty_pos, digit):
@@ -154,16 +160,16 @@ def solve_matrix(matrix):
             if subscribing_functions:
                 notify_subscribers(matrix)
 
-
             if solve_matrix(matrix):
                 return True
             matrix[empty_pos[0]][empty_pos[1]] = "X"
             if subscribing_functions:
                 notify_subscribers(matrix)
 
-        
     return False
-            
+
+# Returns empty position if possible, else returns None.
+
 
 def find_empty_position(matrix):
     for row in matrix:
@@ -174,9 +180,10 @@ def find_empty_position(matrix):
 
     return None
 
-    
 
 if __name__ == "__main__":
+    print("NOTE: THIS IS AN EXAMPLE OUTPUT BASED"
+          " ON CONTENTS OF sudoku.csv.")  # Example
+    fill_game_matrix_from_CSV()  # Example
     solve_matrix(matrix)
     print_matrix()
-
--- sudoku_gui_old.pyw
+++ sudoku_gui_old.pyw
@@ -5,78 +5,52 @@ from string import ascii_uppercase
 import sudoku
 
 
+
+
 class Application(tk.Frame):
 
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.create_widgets()
-
+        
     # Function for creating widgets and creating visual matrix in GUI
     def create_widgets(self):
         self.entries = []
 
         for x in range(0, 9):
-            self.entries.append(
-                [
-                    tk.Button(
-                        self.master,
-                        name=str(x) + str(y),
-                        text="X",
-                        font=(
-                            "Courier",
-                            "12"),
-                        height=2,
-                        width=5,
-                        command=lambda x=x,
-                        y=y: self.change_button_value_dialog(
-                            (x,
-                             y))) for y in range(
-                        0,
-                        9)])
-
+            self.entries.append([tk.Button(self.master, name=str(x)+str(y),  text="X", font=("Courier", "12"), height=2, width=5, command=lambda x = x, y = y: self.change_button_value_dialog((x,y))) for y in range(0, 9)])
+                 
         for x in self.entries:
             for y in x:
                 y.grid(row=self.entries.index(x), column=x.index(y))
-
-        self.run_btn = tk.Button(
-            self.master,
-            text="Run",
-            bg="green",
-            width=5,
-            command=self.run)
+                
+        self.run_btn = tk.Button(self.master, text="Run", bg="green", width=5, command=self.run)
         self.run_btn.grid(row=10, column=4)
 
+        
     # Dialog for changing value of X-marked button in matrix
     def change_button_value_dialog(self, button_widget_coords):
         window = tk.Toplevel()
-        button_widget = self.entries[
-            button_widget_coords[0]][button_widget_coords[1]]
+        button_widget = self.entries[button_widget_coords[0]][button_widget_coords[1]]
         e = tk.Entry(window)
-        btn_ok = tk.Button(
-            window,
-            text="OK",
-            command=lambda: self.change_button_value(
-                window,
-                button_widget,
-                e.get()))
+        btn_ok = tk.Button(window, text="OK", command=lambda: self.change_button_value(window, button_widget, e.get()))
         e.grid(row=0, column=0)
         btn_ok.grid(row=0, column=1)
-
-    # Changes the value of button in matrix
+        
+    # Changes the value of button in matrix    
     def change_button_value(self, window, widget, value):
         try:
             if value != "X":
                 val = int(value)
                 if val <= 0 or val >= 10:
                     raise ValueError
-            widget.configure(text=value)
+            widget.configure(text = value)
             window.destroy()
-        except BaseException:
-            messagebox.showinfo("Invalid character",
-                                "Only numbers between 1-9 or X allowed!")
+        except:
+            messagebox.showinfo("Invalid character", "Only numbers between 1-9 or X allowed!")
             window.focus_force()
-
+            
     # Creates matrix
     def create_matrix(self):
         matrix = []
@@ -85,16 +59,17 @@ class Application(tk.Frame):
             for y in x:
                 indice_list.append(y['text'])
             matrix.append(indice_list)
-
+            
         return matrix
-
-    # Refreshes GUI
+        
+    # Refreshes GUI    
     def update_gui(self, matrix):
         for x1, x2 in zip(self.entries, matrix):
             for y1, y2 in zip(x1, x2):
                 y1.configure(text=y2)
-                self.master.update_idletasks()
-
+                self.master.update_idletasks()         
+                
+            
     def run(self):
         self.run_btn['state'] = "disabled"
         sudoku.set_matrix(self.create_matrix())
@@ -102,6 +77,7 @@ class Application(tk.Frame):
         sudoku.solve_matrix(sudoku.get_matrix())
         self.run_btn['state'] = "normal"
 
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
